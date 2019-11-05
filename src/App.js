c3550671b16d98ff3def085f3574acdfd41bb486
CNLHB <1348844909@qq.com>
2019-11-05 15:50:47 +0800

    第一次提交，初始化文件目录

--- src/App.js
+++ src/App.js
@@ -1,26 +1,29 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-
+import { Row, Col } from 'antd'
+import './style/common.less'
+// import Life from '../src/pages/demo/Life'
+import Footer from './components/Footer/footer'
+import NavLeft from './components/NavLeft/index'
+import Header from './components/Header/header'
+import Content from './components/content/content'
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div>
+            <Row className="container">
+                <Col span={4} className="nav-left">
+                    <NavLeft/>
+                </Col>
+                <Col span={20} className="main">
+                    <Header/>
+                    <Row className="content">
+                        {/* <Home/> */}
+                       <Content></Content>
+                    </Row>
+                    <Footer/>
+                </Col>
+            </Row>
+        </div>
+    );
 }
 
 export default App;
---
+++ src/components/NavLeft/index.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import './index.less'
+
+
+export default class NavLeft extends React.Component {
+    render() {
+        return (
+            <div className="nav-left">
+                <div className="logo"></div>
+                8888888888
+            </div> 
+        )
+    }
+}
